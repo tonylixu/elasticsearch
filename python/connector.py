2c458061fda58941ad38de5355b19a8b88418862
Tony Li Xu <txu@Tonys-MBP.home>
2018-04-29 20:55:40 -0400

    Added requirements.txt file and updated connector.py

--- python/connector.py
+++ python/connector.py
@@ -1,4 +1,9 @@
 # make sure ES is up and running
 import requests
+from elasticsearch import Elasticsearch
 res = requests.get('http://localhost:9200')
 print(res.content)
+
+# Now connect to cluster
+
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
